39b869040eb6c99994c9eea67cbf11c374e9895c
StephenTurk90 <stephenjturk@gmail.com>
2022-09-14 16:48:15 -0400

    updated config/urls.py and library/views.py, created serializers.py and urls.py in library.py

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
+    # path('__debug__/', include('debug_toolbar.urls'))
+    path('', include('library.urls')),
 ]
